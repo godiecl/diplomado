fd63654d3cb4b0e835f55764046d232e0b0fe896
Diego Urrutia-Astorga <mika.cl@gmail.com>
2025-08-30 10:01:07 -0400

    fix: remove the class benchmark

--- libs/benchmark.py
+++ libs/benchmark.py
@@ -9,29 +9,6 @@ from typing import Generator, Optional
 import humanize
 
 
-class Benchmark:
-    def __init__(self, name=None):
-        self.name = name
-
-    def __enter__(self):
-        self.start = time.perf_counter()
-        return self
-
-    def __exit__(self, ty, val, tb):
-        elapsed = time.perf_counter() - self.start
-        name = self.name if self.name else "Block"
-        log.debug(f"{name} executed in {elapsed:.3f} seconds.")
-        return False
-
-    def __call__(self, func):
-        def wrapped(*args, **kwargs):
-            with self:
-                return func(*args, **kwargs)
-            return None
-
-        return wrapped
-
-
 @contextmanager
 def benchmark(
     operation_name: Optional[str] = None, log: Optional[logging.Logger] = None
