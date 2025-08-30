759194f4f269d9fe0a913de58e3409da42dfb5b5
Diego Urrutia-Astorga <mika.cl@gmail.com>
2025-08-30 10:41:25 -0400

    feat: enhance main function with logging and benchmarking

--- scripts/run_hello_world.py
+++ scripts/run_hello_world.py
@@ -1,9 +1,37 @@
 #  Copyright (c) 2025. Diplomado en Inteligencia Artificial Aplicada
+import logging
+import random
+import time
+
+from tqdm import tqdm
+
+from benchmark import benchmark
+from logger import configure_logging
 
 
 def main():
-    print("Hello World 😊")
+    with benchmark("esta es la medicion uno", log):
+        time.sleep(1.5)
+        log.debug("Hello World 😊")
+        time.sleep(0.5)
+        log.warning("Hello World 😊")
+
+    with benchmark("esta es la medicion dos", log):
+        log.info("Hello World 😊")
+        time.sleep(0.5)
+        log.error("Hello World 😊")
+        log.critical("Hello World 😊")
+        log.fatal("Hello World 😊")
+
+    for i in tqdm(range(10_000), ncols=120):
+        sleep = random.uniform(0.01, 0.2)
+        time.sleep(sleep)
 
 
 if __name__ == "__main__":
-    main()
+    configure_logging(log_level=logging.DEBUG)
+
+    log = logging.getLogger(__name__)
+
+    with benchmark("main()", log):
+        main()
